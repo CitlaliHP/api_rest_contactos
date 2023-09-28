f1d9222f1f83bb22881b0a18678983bec70d0665
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-09-28 23:01:08 +0000

    update

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+# Importamos las bibliotecas necesarias
 from typing import Union
 import pandas as pd
 from fastapi import FastAPI
 import json
 
+# Creamos una instancia de la aplicación FastAPI
 app = FastAPI()
 
-# Define a function to read the CSV file and return its data as JSON
+# Definimos una ruta de operación GET en el servidor FastAPI
 @app.get("/v1/contactos")
+# Definimos una función asíncrona para manejar las solicitudes GET a la ruta "/v1/contactos"
 async def read_contactos_csv():
+    # Leemos el archivo CSV usando pandas
     df = pd.read_csv("contactos.csv")
+    # Convertimos el DataFrame de pandas a JSON
     contactos_data = df.to_json(orient="records")
+    # Devolvemos los datos en formato JSON
     return json.loads(contactos_data)
