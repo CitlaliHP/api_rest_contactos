7ad5489e6dbc4f8de5a3be204cbdfe7ac65ee752
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-09-28 16:33:49 +0000

    UPdate

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from typing import Union
-
+import csv
 from fastapi import FastAPI
-
 from asposecells.api import Workbook
 
+import jpype
+jpype.startJVM()
+
+workbook = Workbook("contactos.csv")
+workbook.save("contactos.json")
+
+jpype.shutdownJVM()
+
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
 @app.get("/v1/contactos")
 def read_root():
     return {"Hello": "World"}
+
+# Define a function to read the CSV file and return its data as JSON
+def read_contactos_csv():
+    response = []
+    with open("contactos.csv", newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            response.append(row)
+    return response
