8c377c6f3c403a71e511d38a897b9813da8ad986
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-09-28 16:05:55 +0000

    update

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/personas")
+def get_personas():
+    return {"id": 1,"nombre": "Dejah"}
