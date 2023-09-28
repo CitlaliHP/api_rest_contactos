23964f0146f18a64e9536f034a7a92485043ffb9
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-09-28 22:58:42 +0000

    update

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from typing import Union
-import csv
 import pandas as pd
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
-@app.get("/v1/contactos")
-def read_root():
-    return {"Hello": "World"}
-
 # Define a function to read the CSV file and return its data as JSON
-@app.get("/v1/contactos_csv")
-def read_contactos_csv():
+@app.get("/v1/contactos")
+async def read_contactos_csv():
     df = pd.read_csv("contactos.csv")
-    return df.to_dict(orient='records')
+    contactos_data = df.to_json(orient="records")
+    return json.loads(contactos_data)
