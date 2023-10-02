7bd9a7083dd499c11a7ca4bbc553d46b39ac6594
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-10-02 16:47:19 +0000

    update

--- pruebas/prueba_pydantic/main.py
+++ pruebas/prueba_pydantic/main.py
@@ -9,11 +9,13 @@ class Item(BaseModel):
     tax: float | None = None
 
 
-
 app = FastAPI()
 
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
-
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
