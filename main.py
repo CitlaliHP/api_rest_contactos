557ee54d0954836c205d9b324fcec6d7168f6fb5
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-10-02 15:36:05 +0000

    update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from typing import Union
 import pandas as pd
 from fastapi import FastAPI
 import json
+from pydantic import BaseModel
+
 
 # Creamos una instancia de la aplicación FastAPI
 app = FastAPI()
@@ -25,3 +27,31 @@ async def read_contactos_csv():
     contactos_data = df.to_json(orient="records")
     # Devolvemos los datos en formato JSON
     return json.loads(contactos_data)
+    app = FastAPI()
+
+# Modelo Pydantic para representar un contacto
+class Contacto(BaseModel):
+    nombre: str
+    email: str
+
+# Lista que almacenará los contactos
+contactos_lista = []
+
+# Ruta GET para obtener todos los contactos y mostrar el contenido del archivo CSV
+@app.get("/v1/contactos", response_model=list[Contacto])
+async def get_contactos():
+    # Lee el contenido del archivo CSV
+    try:
+        with open("contactos.csv", newline="", encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            contactos_lista = [{"nombre": row["nombre"], "email": row["email"]} for row in reader]
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="El archivo 'contactos.csv' no se encontró")
+
+    return contactos_lista
+
+# Ruta POST para agregar un nuevo contacto
+@app.post("/v1/contactos", response_model=Contacto)
+async def add_contacto(contacto: Contacto):
+    contactos_lista.append(contacto)
+    return contacto
