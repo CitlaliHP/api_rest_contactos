8beab04cdd83a272439762163b5205003c9a2183
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-10-09 16:22:12 +0000

    update

--- pruebas/archivos/main.py
+++ pruebas/archivos/main.py
@@ -1,39 +1,42 @@
-import os 
+import os
 from typing import Annotated
-
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.post("/files/")
-async def create_files(
-    files: Annotated[list[bytes], File(description="Multiple files as bytes")],
-):
-    return {"file_sizes": [len(file) for file in files]}
-
+# Rutas de destino para PDFs e imágenes
+pdf_path = "pruebas/archivos/static/pdf"
+image_path = "pruebas/archivos/static/imagenes"
 
 @app.post("/uploadfiles/")
 async def create_upload_files(
-    files: Annotated[
-        list[UploadFile], File(description="Multiple files as UploadFile")
-    ],
+    files: Annotated[list[UploadFile], File(description="Multiple files as UploadFile")]
 ):
-    return {"filenames": [file.filename for file in files]}
+    for file in files:
+        # Verificar la extensión del archivo
+        if file.filename.endswith(".pdf"):
+            destination = os.path.join(pdf_path, file.filename)
+        elif file.filename.lower().endswith((".jpg", ".jpeg", ".png", ".gif")):
+            destination = os.path.join(image_path, file.filename)
+        else:
+            return {"error": "Formato de archivo no válido"}
 
+        # Guardar el archivo en la ruta correspondiente
+        with open(destination, "wb") as file_object:
+            file_object.write(file.file.read())
+    
+    return {"message": "Archivo(s) subido(s) exitosamente"}
 
 @app.get("/")
 async def main():
     content = """
-<body>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-</body>
+    <body>
+    <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+    <input name="files" type="file" multiple>
+    <input type="submit">
+    </form>
+    </body>
     """
     return HTMLResponse(content=content)
+    
