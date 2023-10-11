03e99a3837ecd79ad3e11f9b4e9a59f729fd280d
Citlali Hernandez <1722110136@utectulancingo.edu.mx>
2023-10-11 16:14:54 +0000

    update

--- pruebas/imagenes/app.py
+++ pruebas/imagenes/app.py
@@ -3,7 +3,13 @@ im = Image.open("20221126_095059.jpg")
 
 print(im.format, im.size, im.mode)
 
+"""
 box = (0, 0, 400, 400)
 region = im.crop(box)
 
 region.save("recorte.png")
+"""
+
+r, g, b = im.split()
+region = Image.merge("RGB", (b, g, r))
+region.save("cambio.png")
